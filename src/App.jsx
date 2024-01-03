6b714863c725f0a10930818728c5ca837336dc7b
Tania Ali <taniafali@gmail.com>
2024-01-02 20:37:15 -0800

    Finish react static site

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar.jsx"
+import Main from "./components/Main.jsx"
+// import './App.css';
+function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+export default App
